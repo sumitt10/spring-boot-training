870827790249f9b200c1040292a25e64f5ca71e3
Sumit Thigale <98476747+sumitt10@users.noreply.github.com>
2024-02-02 11:56:45 +0530

    Learned an basics of Maven

---
+++ learn-maven/src/test/java/com/example/maven/learnmaven/LearnMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven.learnmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
