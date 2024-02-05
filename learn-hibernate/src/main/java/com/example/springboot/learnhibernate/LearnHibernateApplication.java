e223c2c6f59c2574d67d6158bb55314b0c6c77b1
Sumit Thigale <98476747+sumitt10@users.noreply.github.com>
2024-02-05 18:12:09 +0530

    Exploring Hibernate with JPA - H2 Console

---
+++ learn-hibernate/src/main/java/com/example/springboot/learnhibernate/LearnHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.learnhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnHibernateApplication.class, args);
+	}
+
+}
